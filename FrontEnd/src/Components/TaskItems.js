a085e47d0d80c83cd997c8a05384dfab8ecc054f
sguk18bio <manu27031986@outlook.com>
2020-10-28 00:34:18 +0700

    [Add]: Add Repeater, UserInfoManagement diagrams and specification

--- FrontEnd/src/Components/TaskItems.js
+++ FrontEnd/src/Components/TaskItems.js
@@ -2,64 +2,74 @@
 [Author] - Đỗ Trường Giang 
 [Desc] - Đây là Component TaskItem đùng để hiển thị các task mà người dùng đã thêm vào
 */
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as actions from '../Actions/index';
+import * as actions from "../Actions/index";
 
 class TaskItems extends Component {
-
-/*
+  /*
     [Author] - Đỗ Trường Giang 
     [FunctionName] - onSetStatus
     [Desc] - Đây là hàm dùng để cập nhật lại status mới cho task
     :return: Không có giá trị trả về  
-*/
-    onSetStatus = () => {
-        this.props.toggleStatus(this.props.task.id);
-    };
+  */
+  onSetStatus = () => {
+    this.props.toggleStatus(this.props.task.id);
+  };
 
-/*
+  /*
     [Author] - Đỗ Trường Giang 
     [FunctionName] - onDeleteItem
     [Desc] - Đây là hàm dùng để xóa task
     :return: Không có giá trị trả về  
-*/
-    onDeleteItem = () => {
-        this.props.deleteTask(this.props.task.id);
-        this.props.closeForm();
-    };
+  */
+  onDeleteItem = () => {
+    this.props.deleteTask(this.props.task.id);
+    this.props.closeForm();
+  };
 
-/*
+  /*
     [Author] - Đỗ Trường Giang 
     [FunctionName] - onUpdateItem
     [Desc] - Đây là hàm dùng để cập nhật lại task 
     :return: Không có giá trị trả về   
-*/   
-    onUpdateItem = () => {
-        this.props.openForm();
-        this.props.updateTask(this.props.task);
-    };
+  */
+  onUpdateItem = () => {
+    this.props.openForm();
+    this.props.updateTask(this.props.task);
+  };
 
-    render() {
-        let {task, index} = this.props;
-        return (
-            <tr>
-                <td>{index + 1}</td>
-                <td>{task.name}</td>
-                <td className="text-center" onClick={this.onSetStatus}>
-                    <span className="label label-danger"> {(task.status === true ? 'Active' : 'Hide')} </span>
-                </td>
-                <td className="text-center">
-                    <button type="submit" className="btn btn-primary mr-3" onClick={this.onUpdateItem}>
-                        Update
-                    </button>
-                    <button type="button" className="btn btn-primary" onClick={this.onDeleteItem}>
-                        Finish
-                    </button>
-                </td>
-            </tr>
-        );
-    }
+  render() {
+    let { task, index } = this.props;
+    return (
+      <tr>
+        <td>{index + 1}</td>
+        <td>{task.name}</td>
+        <td className="text-center" onClick={this.onSetStatus}>
+          <span className="label label-danger">
+            {" "}
+            {task.status === true ? "Active" : "Hide"}{" "}
+          </span>
+        </td>
+        <td className="text-center">
+          <button
+            type="submit"
+            className="btn btn-primary mr-3"
+            onClick={this.onUpdateItem}
+          >
+            Update
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={this.onDeleteItem}
+          >
+            Finish
+          </button>
+        </td>
+      </tr>
+    );
+  }
 }
 
 /*
@@ -69,8 +79,8 @@ class TaskItems extends Component {
     :param1: state - array
     :return: Không có giá trị trả về  
 */
-const stateToProps = state => {
-    return {};
+const stateToProps = (state) => {
+  return {};
 };
 
 /*
@@ -82,23 +92,23 @@ const stateToProps = state => {
     :return: Trả về  các props: toggleStatus, deleteTask, closeForm, openForm, updateTask
 */
 const dispatchToProps = (dispatch, props) => {
-    return {
-        toggleStatus: (id) => {
-            dispatch(actions.toggleStatus(id));
-        },
-        deleteTask: (id) => {
-            dispatch(actions.deleteTask(id));
-        },
-        closeForm: () => {
-          dispatch(actions.closeForm());
-        },
-        openForm: () => {
-            dispatch(actions.openForm());
-        },
-        updateTask: (task) => {
-            dispatch(actions.updateTask(task));
-        }
-    };
+  return {
+    toggleStatus: (id) => {
+      dispatch(actions.toggleStatus(id));
+    },
+    deleteTask: (id) => {
+      dispatch(actions.deleteTask(id));
+    },
+    closeForm: () => {
+      dispatch(actions.closeForm());
+    },
+    openForm: () => {
+      dispatch(actions.openForm());
+    },
+    updateTask: (task) => {
+      dispatch(actions.updateTask(task));
+    },
+  };
 };
 
 export default connect(stateToProps, dispatchToProps)(TaskItems);
